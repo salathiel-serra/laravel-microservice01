d5c5cf22e6210d9475a24db591c7695baedd8fbc
Thiel Serra <thiel.serra@gmail.com>
2022-08-10 01:13:28 -0300

    Finalizando API category, com edição e remoção

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -59,22 +59,30 @@ class CategoryController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  string  $url
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StoreOrUpdateCategory $request, $url)
     {
-        //
+        $category = $this->repository->where('url', $url)->firstOrFail();
+
+        $category->update( $request->validated() );
+
+        return response()->json(['message' => 'success']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  string  $url
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($url)
     {
-        //
+        $category = $this->repository->where('url', $url)->firstOrFail();
+
+        $category->delete();
+
+        return response()->json([], 204);
     }
 }
