490d0be33fd7abfe8cc445b67dbc76ccfe8ab7a7
Thiel Serra <thiel.serra@gmail.com>
2022-08-15 19:52:29 -0300

    Registrandp empresas com validação e classe observer

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreOrUpdateCompany;
 use App\Http\Resources\CompanyResource;
 use App\Models\Company;
 
@@ -34,9 +35,11 @@ class CompanyController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreOrUpdateCompany $request)
     {
-        //
+        $company = $this->repository->create($request->all());
+
+        return new CompanyResource($company);
     }
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,10 @@
 namespace App\Providers;
 
 use App\Models\{
-    Category
+    Category, Company
 };
 use App\Observers\{
-    CategoryObserver
+    CategoryObserver, CompanyObserver
 };
 use Illuminate\Support\ServiceProvider;
 
@@ -30,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Category::observe(CategoryObserver::class);
+        Company::observe(CompanyObserver::class);
     }
 }
